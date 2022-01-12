d0f8bf8b3e47463cc243694a248bb6ea76db70b2
Matheus Gomes <mth.gom@gmail.com>
2022-01-12 20:27:09 -0300

    Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import gitLogo from '../../assets/github_logo.svg';
 
 const Repository: React.FC = () => {
   const { username, repositoryname } = useParams();
 
   return (
-    <h1>
-      Repository: {username}/{repositoryname}
-    </h1>
+    <>
+      <Header>
+        <img src={gitLogo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/2254731?v=4"
+            alt="avatar"
+          />
+          <div>
+            <strong>rocketset/unform</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/repository/teste">
+          <div>
+            <strong>Name repository</strong>
+            <p>descrição repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
